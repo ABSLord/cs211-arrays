fbb47b61bf0b4a404ea3e9f9f43eef2371234fa3
Antonio <antonio.maksimovich@gmail.com>
2015-10-04 18:57:20 +0300

    update 3
    исправлены все остальные функции

--- arrays.cpp
+++ arrays.cpp
@@ -1,7 +1,7 @@
-#include <cassert>
 #include <iostream>
+#include <cassert>
 
-
+using namespace std;
 
 void task_1()
 {
@@ -9,61 +9,52 @@ void task_1()
 	int a[n];
 }
 
-// Task 2
 void task_2()
 {
 	int a[5] = { 0 };
 	assert(!a[4]);
 }
 
-// returns zero
+
 int task_3_zero()
 {
-	int a[5];
+	int a[5] = { 0 };
 	assert(!a[4]);
 	return a[4];
 }
 
 void task_4()
 {
-    // Task 4: asserts at the end must hold
-    // Hint: fix with initialization (not assignment)
-    int a[5];
-    
-    assert(a[0] == 1);
-    assert(a[1] == 2);
-    assert(a[2] == 3);
-    assert(a[3] == 4);
-    assert(a[4] == 5);    
+	int a[5] = {1,2,3,4,5};
+	assert(a[0] == 1);
+	assert(a[1] == 2);
+	assert(a[2] == 3);
+	assert(a[3] == 4);
+	assert(a[4] == 5);
 }
 
-// Task 5
 // copy arr1 to arr2
 void task_5_copy(int * arr1, int * arr2, int size)
 {
-    arr2 = arr1;
+	for (int i = 0; i < size; i++)
+		arr2[i] = arr1[i];
 }
 
-// Task 6
 // copy array `arr1` to array `arr2` of the same size
 void task_6_poor_copy(int * arr1, int * arr2)
 {
-    // Hint: something wrong here; test this from main()
-    for(int i = 0; i < sizeof(arr2); ++i)
-    {
-        arr2[i] = arr1[i];
-    }
+
+	for (int i = 0; i <= sizeof(arr1); i++)
+	{
+		arr2[i] = arr1[i];
+	}
 }
 
-// Task 7
 // print array `arr`
 void task_7_print(int * arr, int size, char delim = ' ')
 {
-    // Hint: something wrong here; run this from main()
-    for(int i = 0; i <= size; ++i)
-    {
-        std::cout << a[i] << delim;
-    }
+	for (int i = 0; i < size; ++i)
+		cout << arr[i] << delim;
 }
 
-#endif
+
